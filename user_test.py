18804069ba9fbc61638fdbf9d75d31cfd70519cf
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-11 14:44:52 +0200

    created user object

--- user_test.py
+++ user_test.py
@@ -5,3 +5,11 @@ class TestUnit(unittest.TestCase):
     Args:
     unittest.TestCase:TestCase class that helps in creating test cases
     '''
+
+    # Items up here ......
+
+     def setUp(self):
+         '''
+         Set up method to run before each test cases.
+         '''
+         self.new_user = User("Angela","Karenzi","0787889107","angelakarenzi5@gmail.com") # create user object
