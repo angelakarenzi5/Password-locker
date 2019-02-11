a74cf8d1b3d986cfe522058142721cf5cfb1b641
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-11 17:57:36 +0200

    adding the delete_credentials method

--- credentials.py
+++ credentials.py
@@ -10,3 +10,13 @@ class Credentials:
         self.website_name = website_name
         self.account_name = account_name
         self.password = password
+
+    credentials_list = [] # Empty credentials list
+ # Init method up here
+    def save_credential(self):
+
+        '''
+        save_credential method saves credential objects into credential_list
+        '''
+
+        Credential.credential_list.append(self)
