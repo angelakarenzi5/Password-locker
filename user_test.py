9028f893a38b3c184d8bce85d1399cc128aa57cc
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-11 16:35:10 +0200

    running test 3

--- user_test.py
+++ user_test.py
@@ -22,18 +22,25 @@ class TestUnit(unittest.TestCase):
         self.assertEqual(self.new_User.phone_number,"0787889107")
         self.assertEqual(self.new_User.email,"Angelakarenzi5@gmail.com")
 
-
-if __name__ == '__main__':
-    unittest.main()
-
-
     def test_save_user(self):
         '''
         test_save_user test case to test if the user object is saved into
          the user list
         '''
-        self.new_user.save_user() # saving the new user
+        self.new_User.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
+
+    # Items up here...
+
+        def test_save_multiple_user(self):
+            '''
+            test_save_multiple_user to check if we can save multiple user
+            objects to our user_list
+            '''
+            self.new_user.save_user()
+            test_user = User("Angela","Karenzi","0787889107","angelakarenzi5@gmail.com") # new contact
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)
 
 if __name__ ==  '__main__':
     unittest.main()
