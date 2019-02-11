5e2b55e2ff3e4876455fe7e83d4a72bb63b5dc4e
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-11 19:07:53 +0200

    starting fifth test of find by number

--- Credentials_test.py
+++ Credentials_test.py
@@ -72,6 +72,20 @@ class TestUnit(unittest.TestCase):
 
             self.new_Credentials.delete_Credentials()# Deleting a Credentials object
             self.assertEqual(len(Credentials.Credentials_list),1)
+
+            def test_find_contact_by_number(self):
+        '''
+        test to check if we can find a contact by phone number and display information
+        '''
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0711223344","test@user.com") # new contact
+        test_contact.save_contact()
+
+        found_contact = Contact.find_by_number("0711223344")
+
+        self.assertEqual(found_contact.email,test_contact.email)
+
     
 if __name__ ==  '__main__':
     unittest.main()
