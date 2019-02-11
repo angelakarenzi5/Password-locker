a6cfadd1f6cb9b0cbc2fb2f7dbe102996022dc48
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-11 15:52:29 +0200

    creating another class

--- user_test.py
+++ user_test.py
@@ -1,7 +1,8 @@
-import unittest # Importing the unittest module from unit import Unit #Importing the unit class
-from user import User # Importing the contact class
+import unittest
+from user import User
 class TestUnit(unittest.TestCase):
     '''
+    Test cloass that defines test cases for the user class behaviours.
     Args:
     unittest.TestCase:TestCase class that helps in creating test cases
     '''
@@ -10,17 +11,16 @@ class TestUnit(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_User=User("Angela","Karenzi","0787889107","Angelakarenzi5@gmail.com")
-    # Items up here ......
 
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_user.first_name,"Angela")
-        self.assertEqual(self.new_user.last_name,"Karenzi")
-        self.assertEqual(self.new_user.phone_number,"0787889107")
-        self.assertEqual(self.new_user.email,"angelakarenzi5@gmail.com")
+        self.assertEqual(self.new_User.first_name,"Angela")
+        self.assertEqual(self.new_User.last_name,"Karenzi")
+        self.assertEqual(self.new_User.phone_number,"0787889107")
+        self.assertEqual(self.new_User.email,"Angelakarenzi5@gmail.com")
 
 
 if __name__ == '__main__':
