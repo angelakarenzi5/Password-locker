bd2fbbfc4c65610f0d96969b0c860b2328c33426
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-11 14:37:05 +0200

    first test

--- user_test.py
+++ user_test.py
@@ -1,2 +1,7 @@
 import unittest # Importing the unittest module from unit import Unit #Importing the unit class
 
+class TestUnit(unittest.TestCase):
+    '''
+    Args:
+    unittest.TestCase:TestCase class that helps in creating test cases
+    '''
